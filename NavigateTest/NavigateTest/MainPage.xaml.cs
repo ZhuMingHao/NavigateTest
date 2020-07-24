e80b66fbb5b7faceb093e292aebb14a293b89e49
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2020-07-24 16:00:38 +0800

    Add project files.

---
+++ NavigateTest/NavigateTest/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigateTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (Navigation.ModalStack.Where(a => a is MainPage).FirstOrDefault() != null)
+            {
+                CloseBtn.IsVisible = true;
+            }
+
+        }
+        private async void CloseClick(object sender, EventArgs e)
+        {
+
+            await Navigation.PopModalAsync();
+        }
+    }
+}
---
+++ NavigateTest/NavigateTest/Page5.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigateTest
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page5 : ContentPage
+	{
+		public Page5 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.RemovePage( Navigation.NavigationStack.Where(a=> a is Page4).FirstOrDefault());
+        }
+
+        private void PushClick(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+            foreach (var item in Navigation.NavigationStack)
+            {
+                System.Diagnostics.Debug.WriteLine(item.GetType().Name);
+            }
+        }
+    }
+}
